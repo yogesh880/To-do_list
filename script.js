d8b43cab68c158f89294fcafc87b33228ab04dd1
yogesh880 <85214154+yogesh880@users.noreply.github.com>
2025-09-13 12:56:51 +0530

    to_do_list

---
+++ script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded',()=>{
+let inputtext=document.getElementById('todo-input');
+let addbtn=document.getElementById('add-task-btn');
+let taskList= document.getElementById('todo-list');
+let taskArray=JSON.parse(localStorage.getItem('task'))||[];
+
+ taskArray.forEach((task)=>renderTask(task)
+ );
+
+addbtn.addEventListener('click',()=>{
+  let tasktext=inputtext.value.trim();
+  if (tasktext==='') return;
+  let newTask={
+    id:Date.now(),
+    task :tasktext,
+    completed: false
+  }
+  taskArray.push(newTask);
+  renderTask(newTask); 
+  inputtext.Value=''
+  // console.log(taskArray);
+  Save(taskArray);
+})
+
+function renderTask(task){
+ console.log(task.task);
+ const li = document.createElement('li');
+ li.setAttribute('data-id',task.id)
+ if (task.completed){
+  li.classList.add('completed');
+ }
+ 
+ li.innerHTML=`
+ <span>${task.task}</span>
+ <button>delete</button>`
+ taskList.appendChild(li);
+ 
+ li.addEventListener('click',(e)=>{
+  if (e.target.tagName==='BUTTON') return;
+  task.completed=!task.completed;
+  li.classList.toggle('completed');
+  Save(taskArray);
+ })
+
+ li.querySelector('BUTTON').addEventListener('click',(e)=>{
+   e.stopPropagation(); //to prevent triggering listener on parrents.
+  taskArray=taskArray.filter((t)=>t.id!=task.id);
+  console.log(taskArray);
+   li.remove();
+   Save(taskArray);
+ })
+}
+function Save(tasks){ 
+  localStorage.setItem('task',JSON.stringify(tasks));
+}
+
+});
